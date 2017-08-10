5e565bca1abb11d31a900085a99ba87786203641
[wanglong521] <[18813152360m0@sina.cn]>
2017-08-10 20:21:47 +0800

    修改了

--- app/src/main/java/com/example/admin/aaa/MainActivity.java
+++ app/src/main/java/com/example/admin/aaa/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.admin.aaa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ExpandableListView;
 
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity  {
         for (int i=0;i<templist.size();i++){
             childlist.add(templist);
         }
+        Log.e("TAG", "initData: "+"s" );
         MyAdapter my=new MyAdapter(grouplist,childlist,MainActivity.this);
         myexpadlist.setAdapter(my);
     }
